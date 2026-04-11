2c0b42e16e5a3bff6faa865ce990a11aef94746d
satishskid <satish@skids.health>
2026-04-11 22:44:03 +0530

    fix(analytics): complete Meta Pixel install
    Support the existing Meta pixel env keys and add the noscript fallback so the website includes the full pixel snippet site-wide.

--- src/components/analytics/AnalyticsScripts.tsx
+++ src/components/analytics/AnalyticsScripts.tsx
@@ -3,7 +3,10 @@ import Script from "next/script";
 
 export default function AnalyticsScripts() {
     const gaId = process.env.GOOGLE_ANALYTICS_ID;
-    const fbPixelId = process.env.FACEBOOK_PIXEL_ID;
+    const fbPixelId =
+        process.env.META_PIXEL_ID ||
+        process.env.META_CAPI_PIXEL_ID ||
+        process.env.FACEBOOK_PIXEL_ID;
 
     return (
         <>
@@ -27,20 +30,32 @@ export default function AnalyticsScripts() {
             )}
 
             {fbPixelId && (
-                <Script id="facebook-pixel" strategy="afterInteractive">
-                    {`
-                    !function(f,b,e,v,n,t,s)
-                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                    n.queue=[];t=b.createElement(e);t.async=!0;
-                    t.src=v;s=b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t,s)}(window, document,'script',
-                    'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '${fbPixelId}');
-                    fbq('track', 'PageView');
-                    `}
-                </Script>
+                <>
+                    <Script id="facebook-pixel" strategy="afterInteractive">
+                        {`
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '${fbPixelId}');
+                        fbq('track', 'PageView');
+                        `}
+                    </Script>
+                    <noscript>
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
+                            height="1"
+                            width="1"
+                            style={{ display: "none" }}
+                            src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`}
+                            alt=""
+                        />
+                    </noscript>
+                </>
             )}
         </>
     );
